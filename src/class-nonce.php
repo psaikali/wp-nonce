91a28f4dff69674dd6ed90eabb9f9aa8d94d0c8c
Pierre Saïkali <dev@mosaika.fr>
2018-09-28 09:38:55 +0200

    Documentation comments for the Nonce class.

--- src/class-nonce.php
+++ src/class-nonce.php
@@ -2,7 +2,17 @@
 
 namespace Inpsyde\Nonce;
 
+/**
+ * Default Nonce class.
+ * 
+ * Allows user to create a basic WordPress nonce.
+ */
 class Nonce extends Nonce_Common {
+	/**
+	 * Generate the nonce.
+	 *
+	 * @return string $nonce The generated nonce string.
+	 */
 	public function create() {
 		$nonce = wp_create_nonce( $this->getAction() );
 		$this->setNonce( $nonce );
@@ -10,6 +20,15 @@ class Nonce extends Nonce_Common {
 		return $this->getNonce();
 	}
 	
+	/**
+	 * Verify if a nonce is valid.
+	 *
+	 * @param string $value (optional) The value to verify against the generated nonce.
+	 *                                 If no value is passed, the generated nonce will be used to check 
+	 *                                 if it is still valid, you know, just in case ¯\_(ツ)_/¯.
+	 * @return false|int False if the nonce is invalid, 1 if the nonce is valid and generated between
+	 *                   0-12 hours ago, 2 if the nonce is valid and generated between 12-24 hours ago.
+	 */
 	public function isValid( $value = null ) {
 		if ( is_null( $value ) ) {
 			$value = $this->getNonce();
